28fc8d3887097de1b6db3b147f9f99d6cbdac02d
Prajna <prajnapprabhu3@gmail.com>
2021-10-30 00:12:13 +0530

    Initial Commit

---
+++ Literals.java
@@ -0,0 +1,16 @@
+package com.Prajna;
+
+public class literals4 {
+    public static void main(String[] args){
+        byte age=34;
+        int age2=14;
+        short age3=33;
+        char ch='A';
+        float f1=5.6f;
+        double d1=4.66D;
+        boolean a=true;
+        System.out.println(age);
+        String name="Prajna";
+        System.out.println(name);
+    }
+}
---
+++ StringMethods.java
@@ -0,0 +1,13 @@
+package com.Prajna;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        String name="Prajna";
+//        String n= new String("Prajna");
+        System.out.println(name);
+        int length= name.length();
+        System.out.println(length);
+
+
+    }
+}
